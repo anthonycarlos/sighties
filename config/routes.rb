36b53eb348b87fe7b5ed40d742ea3c32bafbecbc
anthonycarlos <anthony@carlosfamily.com>
2013-04-01 06:34:28 -0400

    Add user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Sighties::Application.routes.draw do
+  devise_for :users
+
   resources :words
 
-
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
